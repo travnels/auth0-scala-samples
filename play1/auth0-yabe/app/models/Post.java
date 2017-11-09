032634f090c1af1e3dd83acefc5f80f2c217dd2a
Travis Nelson <travis.o.nelson@gmail.com>
2017-11-09 14:44:56 -0700

    basic administration area using CRUD

--- play1/auth0-yabe/app/models/Post.java
+++ play1/auth0-yabe/app/models/Post.java
@@ -4,24 +4,31 @@ import java.util.*;
 import javax.persistence.*;
  
 import play.db.jpa.*;
+import play.data.validation.*;
  
 @Entity
 public class Post extends Model {
  
+    @Required
     public String title;
+    
+    @Required
     public Date postedAt;
     
     @Lob
+    @Required
+    @MaxSize(10000)
     public String content;
     
+    @Required
     @ManyToOne
     public User author;
-  
-   	@OneToMany(mappedBy="post", cascade=CascadeType.ALL)
-	public List<Comment> comments;
-
-	@ManyToMany(cascade=CascadeType.PERSIST)
-	public Set<Tag> tags;
+    
+    @OneToMany(mappedBy="post", cascade=CascadeType.ALL)
+    public List<Comment> comments;
+    
+    @ManyToMany(cascade=CascadeType.PERSIST)
+    public Set<Tag> tags;
 	 
 	public Post(User author, String title, String content) {
 	    this.comments = new ArrayList<Comment>();
@@ -56,5 +63,9 @@ public class Post extends Model {
 	    return Post.find(
 	            "select distinct p from Post p join p.tags as t where t.name in (:tags) group by p.id, p.author, p.title, p.content,p.postedAt having count(t.id) = :size"
 	    ).bind("tags", tags).bind("size", tags.length).fetch();
+	}
+
+	public String toString() {
+	    return title;
 	}
 }
